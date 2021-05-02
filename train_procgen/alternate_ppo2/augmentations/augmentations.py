18c2f81ad68bc16a547d672ba85d1d3e45e8d54e
Frank <fklin@berkeley.edu>
2021-05-02 02:28:50 -0700

    translate_x push

--- train_procgen/alternate_ppo2/augmentations/augmentations.py
+++ train_procgen/alternate_ppo2/augmentations/augmentations.py
@@ -134,7 +134,7 @@ def sharpness(pil_img, level):
     return ImageEnhance.Sharpness(pil_img).enhance(level)
 
 
-augmentations = [shear_y]
+augmentations = [translate_x]
 
     
 unused_augmentations = [autocontrast, equalize, posterize, rotate, solarize, shear_x, shear_y,
